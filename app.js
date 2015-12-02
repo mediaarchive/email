2806a66240c310d022594d30007bceaff5fe2d89
atnartur <atnartur2012@gmail.com>
2015-12-02 22:49:51 +0300

    + парсинг писем

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var inbox = require("inbox");
 var iconv = require('iconv-lite');
 var async = require('async');
 var fs = require('fs');
+var MailParser = require("mailparser").MailParser;
 
 iconv.extendNodeEncodings();
 
@@ -24,17 +25,31 @@ imap.on("connect", function(){
     console.log("Successfully connected to server");
 
     imap.openMailbox('INBOX', function(err, info){
-        imap.search({unseen: true}, function(err, emails){
+        imap.search({unseen: true}, true, function(err, emails){
 
             async.eachSeries(emails, function (item, callback) { // iterator
+                console.log(emails, item);
                 var stream = imap.createMessageStream(item);
                 var string = '';
                 stream.on('data',function(buffer){
-                    string += buffer.toString('win1251');
+                    string += buffer;
                 });
 
                 stream.on('end',function(){
-                    fs.writeFile('temp/' + (new Date()).getTime() + '.tmp', string);
+                    //fs.writeFile('temp/' + (new Date()).getTime() + '.tmp', string);
+
+                    var mailparser = new MailParser();
+
+                    mailparser.on("end", function(mail_object){
+                        console.log("From:", mail_object.from); //[{address:'sender@example.com',name:'Sender Name'}]
+                        console.log("Subject:", mail_object.subject); // Hello world!
+                        console.log("Text body:", mail_object.text); // How are you today?
+                        console.log(mail_object.attachments )
+                    });
+
+                    mailparser.write(string);
+                    mailparser.end();
+
                     console.log('processed');
                     callback();
                 });
